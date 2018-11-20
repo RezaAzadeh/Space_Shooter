a4fb7185cb25efb4db5d5ff30e0458f6b8949229
RezaAzadeh <azadeh.reza@gmail.com>
2018-11-20 08:48:37 +0330

    Hazards added

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour 
+{
+
+    public float tumble;
+	
+	void Start () 
+    {
+        this.GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * tumble;
+	}
+	
+	
+	void Update () 
+    {
+		
+	}
+}
