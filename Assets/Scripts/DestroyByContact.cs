c81707f6ef238e890aa09b4de2d818b4b5ff4593
RezaAzadeh <azadeh.reza@gmail.com>
2018-11-21 10:47:03 +0330

    Score Count Added

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -6,14 +6,28 @@ public class DestroyByContact : MonoBehaviour
 {
     public GameObject explosion;
     public GameObject playerExplosion;
+    public int scoreValue;
+    private GameController gameController;
+
+    void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if (gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Boundary")
             return;
         Instantiate(explosion, transform.position, transform.rotation);
+
         if(other.tag == "Player")
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+
+        gameController.AddScore(scoreValue);
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
